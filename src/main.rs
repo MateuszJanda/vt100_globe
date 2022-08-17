57ab33d43cd74c1cacbfcb02e8f14596e9fcb474
Mateusz Janda <mateusz.janda@gmail.com>
2022-08-17 18:35:07 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// use std::time::Duration;
+// use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    // sleep(Duration::from_millis(100)).await;
+    println!("100 ms have elapsed");
+}
